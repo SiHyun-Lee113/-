b50e91707c43d60b9d33f95a4d261dd61495a73e
SiHyun-Lee113 <tlgus0113@gmail.com>
2021-10-20 17:47:32 +0900

    test

---
+++ app/src/main/java/com/example/team_project/SearchActivity.java
@@ -0,0 +1,122 @@
+package com.example.team_project;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+
+    //member variables
+    private EditText searchInput;
+    private ListView videosFound;
+    private Handler handler;
+    private List<VideoItem> searchResults;
+    private String str;
+    private String test;
+
+    private ImageView btn_home;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment1);
+
+        searchInput = (EditText) findViewById(R.id.search_input);
+        videosFound = (ListView) findViewById(R.id.list_view_search);
+        handler = new Handler();
+
+
+        //listen for user input
+
+
+        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+                str = searchInput.getText().toString();
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    searchOnYoutube (str);
+                    return false;
+                }
+                return true;
+            }
+        });
+
+        addClickListener();
+
+        btn_home = (ImageView) findViewById(R.id.go_home_search);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //perform search on background thread
+    private void searchOnYoutube(final String keywords) {
+        new Thread() {
+            public void run() {
+                YoutubeConnector yc = new YoutubeConnector(SearchActivity.this);
+                searchResults = yc.search(keywords);
+                handler.post(new Runnable() {
+                    public void run() {
+                        updateVideosFound();
+                    }
+                });
+            }
+        }.start();
+    }
+
+    private void updateVideosFound() {
+        ArrayAdapter<VideoItem> adapter = new ArrayAdapter<VideoItem>(getApplicationContext(), R.layout.video_item, searchResults) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = getLayoutInflater().inflate(R.layout.video_item, parent, false);
+                }
+                ImageView thumbnail = (ImageView) convertView.findViewById(R.id.video_thumbnail);
+                TextView title = (TextView) convertView.findViewById(R.id.video_title);
+                TextView description = (TextView) convertView.findViewById(R.id.video_description);
+
+                VideoItem searchResult = searchResults.get(position); //
+
+                Glide.with(getApplicationContext()).load(searchResult.getThumbnailURL()).into(thumbnail);
+
+                title.setText(searchResult.getTitle());
+                description.setText(searchResult.getDescription());
+                return convertView;
+            }
+        };
+
+        videosFound.setAdapter(adapter);
+    }
+
+    private void addClickListener() {
+        videosFound.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplication(), Search.class);
+                intent.putExtra("VIDEO_ID", searchResults.get(position).getId());
+                intent.putExtra("title", searchResults.get(position).getTitle());
+                startActivity(intent);
+            }
+        });
+    }
+}
